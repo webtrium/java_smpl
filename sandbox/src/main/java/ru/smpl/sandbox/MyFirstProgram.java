78b6bd12b528544cefda9505464f34a06d565445
webtrium <webtrium@mail.ru>
2017-03-14 21:28:34 +0400

    Вариант 2 решения задачи расчета расстояния между двумя точками на плоскости

--- sandbox/src/main/java/ru/smpl/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/smpl/sandbox/MyFirstProgram.java
@@ -4,9 +4,10 @@ public class MyFirstProgram {
 
   public static void main (String[] args) {
 
-    Point p = new Point(1,1,2,2);
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(2,2);
 
     System.out.printf("Расстояние между двумя точками на плоскости, с координатами: " +
-            "(%.1f;%.1f) и (%.1f;%.1f) равно: %.2f!", p.dotX1, p.dotY1, p.dotX2, p.dotY2, p.distance());
+            "(%.1f;%.1f) и (%.1f;%.1f) равно: %.2f!", p1.dotX, p1.dotY, p2.dotX, p2.dotY, p1.distance(p2));
   }
 }
--- sandbox/src/main/java/ru/smpl/sandbox/Point.java
+++ sandbox/src/main/java/ru/smpl/sandbox/Point.java
@@ -2,20 +2,16 @@ package ru.smpl.sandbox;
 
 public class Point {
 
-    public double dotX1;
-    public double dotY1;
-    public double dotX2;
-    public double dotY2;
+    public double dotX;
+    public double dotY;
 
-    public Point(double dotX1, double dotY1, double dotX2, double dotY2){
-        this.dotX1 = dotX1;
-        this.dotY1 = dotY1;
-        this.dotX2 = dotX2;
-        this.dotY2 = dotY2;
+    public Point(double dotX, double dotY){
+        this.dotX = dotX;
+        this.dotY = dotY;
     }
 
-    public double distance(){
-        return Math.sqrt(Math.pow((this.dotX1-this.dotX2),2) + Math.pow((this.dotY1-this.dotY2),2));
+    public double distance(Point p){
+        return Math.sqrt(Math.pow((this.dotX - p.dotX),2) + Math.pow((this.dotY - p.dotY),2));
     };
 
 }
