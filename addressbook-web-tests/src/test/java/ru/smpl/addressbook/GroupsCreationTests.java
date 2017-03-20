7449dffcf4af9a119f90666adff93896f77cde81
webtrium <webtrium@mail.ru>
2017-03-21 02:24:09 +0400

    Во вспомогательный метод выделен метод заполнения формы

--- addressbook-web-tests/src/test/java/ru/smpl/addressbook/GroupsCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/smpl/addressbook/GroupsCreationTests.java
@@ -16,10 +16,6 @@ public class GroupsCreationTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupsCreationTests() {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -28,6 +24,10 @@ public class GroupsCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupsCreation() {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
