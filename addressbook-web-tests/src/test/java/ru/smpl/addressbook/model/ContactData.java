224af651921021cb7deb826e2d19ca3dfa1e6d26
webtrium <yatrusov@mts.ru>
2017-04-06 17:00:53 +0300

    Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/smpl/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/smpl/addressbook/model/ContactData.java
@@ -28,4 +28,31 @@ public class ContactData {
     public String getGroup() {
         return group;
     }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+        return email != null ? email.equals(that.email) : that.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstname != null ? firstname.hashCode() : 0;
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
