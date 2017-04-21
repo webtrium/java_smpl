5be88c3a67b518fa8107866bcddaaa43e3590182
webtrium <yatrusov@mts.ru>
2017-04-21 18:36:56 +0300

    Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/smpl/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/smpl/addressbook/tests/TestBase.java
@@ -1,12 +1,21 @@
 package ru.smpl.addressbook.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.smpl.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.openqa.selenium.remote.BrowserType.*;
 
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", CHROME));
@@ -16,9 +25,18 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
 }
